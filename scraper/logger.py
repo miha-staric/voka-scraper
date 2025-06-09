81e2545fb81298908442636d0047391a096a7f9c
mihor <mihor@pm.me>
2025-06-09 11:53:20 +0200

    Clean the comments

--- scraper/logger.py
+++ scraper/logger.py
@@ -5,21 +5,18 @@ import os
 # Create logger
 logger = logging.getLogger("scraper")
 logger.setLevel(config.LOG_LEVEL)
-
-# Prevent duplicate logs in some environments (like Jupyter)
 logger.propagate = False
-
-# Define formatter
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-# Console handler (already what you had)
+# Console handler
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
-# File handler (this is new)
+# File handler
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 file_handler = logging.FileHandler(f"{log_dir}/scraper.log")
 file_handler.setFormatter(formatter)
+
 logger.addHandler(file_handler)
